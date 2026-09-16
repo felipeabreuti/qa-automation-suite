8c7365ad8d1859eabbfa4326bd09373b20f13cf3
Felipe Abreu <felipeabreu.ti@gmail.com>
2026-09-16 15:20:09 -0300

    feat: add load test script with summary reporting

---
+++ tests-load/load-test.js
@@ -0,0 +1,22 @@
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.1.0/index.js';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { config } from './src/config.js';
+import { userFlow } from './src/scenarios.js';
+
+export const options = {
+  stages: config.stages,
+  thresholds: config.thresholds,
+};
+
+export default function () {
+  userFlow();
+}
+
+export function handleSummary(data) {
+  return {
+    'report/summary.html': htmlReport(data),
+    'report/summary.json': JSON.stringify(data, null, 2),
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+  };
+}
+
